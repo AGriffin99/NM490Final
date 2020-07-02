1aff035cacdd365b6bc7a3c6081bf4af24cd350b
Anitra Griffin <anitra.griffin99@gmail.com>
2020-07-02 10:49:04 -0400

    Updates

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-function 
+var urls =[
+  'kimberlybryant.html',
+  'about.html',
+  'reshmasaujani.html'
+
+];
+
+function rando(){
+  var url = urls[Math.floor(Math.random()*urls.length)];
+  window.location=url;
+}
